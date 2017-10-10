cea23a4b71880074301fb67643d149f3c9726bc7
Florent Callaou <florent.callaou22@gmail.com>
2017-10-10 10:30:02 +0200

    commit initial

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,12 @@
+package ex1;
+
+public class Calcul {
+
+	public int add(int a, int b){
+		return a + b;
+	}
+	
+	public int substract(int a, int b){
+		return a - b;
+	}
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,18 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+	public int siret;
+	public String name;
+	public String adress;
+	public Date creationDate;
+	
+	public static final int capitalMax = 3_000_000;
+	
+	public void display(){
+		
+	}
+	
+}
